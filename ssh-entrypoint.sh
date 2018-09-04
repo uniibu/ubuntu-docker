96198af342e2bc05cb8c6fea21c897069364563a
Uni Sayo <unidev@bitclimb.com>
2018-09-04 12:42:12 +0800

    Update ssh-entrypoint.sh

--- ssh-entrypoint.sh
+++ ssh-entrypoint.sh
@@ -22,6 +22,14 @@ file_env() {
 file_env 'UBUNTU_USER'
 file_env 'UBUNTU_PW'
 
+if [ -z "$UBUNTU_USER" ]; then
+  UBUNTU_USER=ubuntu
+fi
+
+if [ -z "$UBUNTU_PW" ]; then
+  UBUNTU_PW=ubuntu
+fi
+
 useradd -u 1000 -G users,sudo,root -d /home/$UBUNTU_USER --shell /bin/bash -m $UBUNTU_USER && \
 usermod -p $UBUNTU_PW $UBUNTU_USER
 
