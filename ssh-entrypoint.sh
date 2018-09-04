e622bc7c8c6236304de2c315ef7a6a45f531f745
Uni Sayo <unidev@bitclimb.com>
2018-09-04 12:36:05 +0800

    Create ssh-entrypoint.sh

---
+++ ssh-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'UBUNTU_USER'
+file_env 'UBUNTU_PW'
+
+useradd -u 1000 -G users,sudo,root -d /home/$UBUNTU_USER --shell /bin/bash -m $UBUNTU_USER && \
+usermod -p $UBUNTU_PW $UBUNTU_USER
+
+exec "$@"
